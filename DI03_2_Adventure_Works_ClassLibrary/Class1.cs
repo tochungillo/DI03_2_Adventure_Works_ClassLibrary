f1c593430331b5d4de642c1699925c8bb90d2d41
tochungillo <andy98lopez@gmail.com>
2021-02-03 18:48:11 +0100

    1r commit Creacion de la class library

---
+++ DI03_2_Adventure_Works_ClassLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DI03_2_Adventure_Works_ClassLibrary
+{
+    public class Class1
+    {
+    }
+}
