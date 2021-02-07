34fb7adabd1b65717078c9a7aeb2d103475f9944
tochungillo <andy98lopez@gmail.com>
2021-02-07 20:17:46 +0100

    5to commit agregada la funcionalidad para que busque product model aleatoreos al presionar la imagen y le asigna la imagen obtenida de la base de datos, crea botones por cada size del productModel

--- DI03_2_Adventure_Works_ClassLibrary/DI03_2_Control.cs
+++ DI03_2_Adventure_Works_ClassLibrary/DI03_2_Control.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,26 +13,99 @@ namespace DI03_2_Adventure_Works_ClassLibrary
 {
     public partial class DI03_2_Control : UserControl
     {
+        // almacena los productModelID
+        public List<int> modelIDs = new List<int>();
+
+        public int posicion;
+        public int productID;
+        public int min;
+        public int max;
+
+        Random random = new Random();
+        DataAccess da = new DataAccess();
+
         public DI03_2_Control()
         {
             InitializeComponent();
             // tooltip shown on mantain mouse hover productImagePictureBox
             instructionsToolTip.SetToolTip(productImagePictureBox, "Click to display another product");
         }
-        // cursor change to hand on enter productImagePictureBox
+
+        // Cursor change to hand on enter productImagePictureBox
         private void productImagePictureBox_MouseEnter(object sender, EventArgs e)
         {
             Cursor = Cursors.Hand;
         }
-        // cursor change to default on leave productImagePictureBox
+
+        // Cursor change to default on leave productImagePictureBox
         private void productImagePictureBox_MouseLeave(object sender, EventArgs e)
         {
             Cursor = Cursors.Default;
         }
 
+        // Devuelve un valor aleatorio entre min y max
+        public int Aleatorio()
+        {
+            return random.Next(min, max);
+        }
+
         private void productImagePictureBox_Click(object sender, EventArgs e)
         {
+            posicion = Aleatorio();
 
+            // Borra si hay otros botones creados
+            sizesFlowLayoutPanel.Controls.Clear();
+
+            // Obtiene los productos
+            List<ProductModel> products = da.GetProductModels(modelIDs[posicion]);
+
+            // pides que esten ordenados de mayor a menor por size
+            try
+            {
+                // Si es un numero podra hacer la conversion
+                int esnumero = int.Parse(products[0].Size);
+                // Entonces ordena de mayor a menor (por ejemplo: 38, 42, 46 ...)
+                products = (List<ProductModel>) products.OrderBy(x => x.Size).ToList();
+            }
+            catch (Exception ex)
+            {
+                // Si es una letra, el orden es inverso (debe ser: S, M, L, XL)
+                products = (List<ProductModel>) products.OrderByDescending(x => x.Size).ToList();
+            }
+
+            // Convierte los datos a una imagen y la asigna a productImagePictureBox
+            MemoryStream ms = new MemoryStream(products[0].LargePhoto);
+            Image foto = Image.FromStream(ms);
+            productImagePictureBox.Image = foto;
+
+            // Muestra la informacion del product model en productModelDataTextBox
+            productModelDataTextBox.Text = products[0].ToString();
+
+            // Creara botones y los introducira en sizesFlowLayoutPanel
+            GenerarSizes();
+        }
+
+        public void GenerarSizes()
+        {
+            // Almacena los distintos sizes del productModel y el productID que le corresponde
+            List<ProductAndSize> sizes = da.GetProductAndSizes(modelIDs[posicion]);
+
+            // Ha de crear botones por cada size
+            foreach (ProductAndSize ps in sizes)
+            {
+                Button buttonSize = new Button();
+                if (ps.Size == null)
+                {
+                    buttonSize.Text = "NULL";
+                }
+                else
+                {
+                    buttonSize.Text = ps.Size;
+                }
+                buttonSize.Name = ps.ProductID.ToString();
+                // introduces esos botones al sizesFlowLayoutPanel
+                sizesFlowLayoutPanel.Controls.Add(buttonSize);
+            }
         }
     }
 }
--- DI03_2_Adventure_Works_ClassLibrary/ProductModel.cs
+++ DI03_2_Adventure_Works_ClassLibrary/ProductModel.cs
@@ -7,6 +7,8 @@
         public double ListPrice { get; set; }
         public byte[] LargePhoto { get; set; }
 
+        public string Size { get; set; }
+
         public override string ToString()
         {
             return $"Product model id: {ProductModelID}, Product model name: {ProductModelName}, List price: {ListPrice}";
